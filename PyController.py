b66ea876048843ee26749dffc3c65e5f38f1957c
benmccornack <147429156+benmccornack@users.noreply.github.com>
2024-04-16 10:23:13 -0700

    Modified the pycontroller to turn only the PV loads on and off. As well as increased the damping coefficient of the generator to reduce oscillations.
    Looking for a way to add a controller on top of that
    source to make it more realistic.

--- PyController.py
+++ PyController.py
@@ -30,11 +30,12 @@ for iload in dss.ActiveCircuit.Loads.AllNames:
     temp["kvar"] = float(myString)
 
     temp["PVName"] = "PV"+temp["Bus1"]
+    temp["kw"] = -1
     tempName.append(temp["PVName"])
-    if count < 20: # I used generators origionally and adding 2000 generators was causing the calculations to diverge so this was to limit that, it is an artifact of past times but im leaving just incase i need it
+    if count > -20: # I used generators origionally and adding 2000 generators was causing the calculations to diverge so this was to limit that, it is an artifact of past times but im leaving just incase i need it
         #dss.Text.Command = f"New   "Generator.{temp["PVName"]}"  Bus1={temp["Bus1"]}  kW=3 PF=1  kVA=3  kV={temp["kv"]}  Xdp=0.27  Xdpp=0.2  H=2 Conn=Delta"
-        dss.Text.Command = f'New Load.{temp["PVName"]} Bus1={temp["Bus1"]} Phases=1 Conn=Wye Model=1 kV={temp["kv"]} kW=0'
-        #count += 1
+        dss.Text.Command = f'New Load.{temp["PVName"]} Bus1={temp["Bus1"]} Phases=1 Conn=Wye Model=1 kV={temp["kv"]} kW={temp["kw"]}'
+        count += 1
 
     LoadInformation.append(temp)
     p = 1
@@ -42,36 +43,44 @@ for iload in dss.ActiveCircuit.Loads.AllNames:
 loadDF = pd.DataFrame(LoadInformation)
 
 
-dss.ActiveCircuit.Solution.MaxIterations = 200
-dss.ActiveCircuit.Solution.MaxControlIterations = 300
-dss.Text.Command = "Solve"
+dss.ActiveCircuit.Solution.MaxIterations = 300
+dss.ActiveCircuit.Solution.MaxControlIterations = 400
+
+dss.Text.Command = f"Solve Number=5"
+
+dss.Text.Command = "Set mode=dynamic h=0.005 number=10"
+iterations = 1
+steps = 5000
+dss.Text.Command = f"Solve Number=1500"
 if dss.ActiveCircuit.Solution.Converged == True: # need to check if the "PV" i added cause the program to diverge.
-    dss.Text.Command = "Set mode=dynamic h=0.001 number=1000"
+    #dss.Text.Command = "Set mode=dynamic h=0.005 number=10"
     iterations = 1
-    steps = 5000
+    #steps = 500
+    dss.Text.Command = f"Solve Number={steps*3}"
+    
     for i in range(iterations): #cycle power supply attack
-        #Turn off every power supply
-        for load_name in dss.ActiveCircuit.Loads.AllNames:
-            dss.Text.Command = f"Edit Load.{load_name} kW=0"
+        #Turn off the PV loads    
+        for index,loadName in loadDF.iterrows():
+            name = loadName["PVName"]
+            kw   = loadName["kw"]
+            dss.Text.Command = f"Edit Load.{name} kW={kw*0.6}"
         dss.Text.Command = f"Solve Number={steps}"
         
-        #Turn them back on
-        for load_name in dss.ActiveCircuit.Loads.AllNames:
-            # Restore the original kW value from the dataframe
-            original_kw = loadDF.loc[loadDF['PVName'] == load_name]['kw'].values
-            dss.Text.Command = f"Edit Load.{load_name} kW={original_kw}"
+        #Turn on the PV loads
+        for index,loadName in loadDF.iterrows():
+            name = loadName["PVName"]
+            kw   = loadName["kw"]
+            dss.Text.Command = f"Edit Load.{name} kW={kw*1}"
         dss.Text.Command = f"Solve Number={steps}"
-    dss.Text.Command = "New fault.F1 phases=3 Bus1=m1209749"
-    dss.Text.Command = "Solve number=63"
-    dss.Text.Command = "Disable Fault.F1"
-    dss.Text.Command = "Open Line.LN5896826-1"
-    dss.Text.Command = "Solve Number=3914"
-
+    
+    #dss.Text.Command = f"Solve Number={steps/4}"
+    
     dss.Plotting.enable(show=False)
     dss.Text.Command = "Plot Circuit Power Max=9000 dots=n labels=n  C1=Blue  1ph=3   ! $00FF0000"
     #dss.Text.Command = "plot circuit Losses Max=50 dots=n labels=n subs=y C1=Blue"
     dss.Text.Command = "plot profile ph=all"
-    dss.Text.Command = "Plot monitor object= dpq"
+    dss.Text.Command = "Plot monitor object= dpq channel=1"
+    dss.Text.Command = "Plot monitor object= G_m1125934_gen"
 
     dss.Plotting.enable(show=True)
     dss.Text.Command = "plot"
